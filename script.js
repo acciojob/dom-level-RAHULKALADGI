5e3672c7d7b0bdbd3e0842540daf15fd878001cb
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-21 21:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const ul = document.getElementByTagName("ul")[0];
+const ul = document.getElementsByTagName("ul")[0];
 let displayText;
 for(let i = 0;i<ul.children.length;i++) {
 	let node = ul.children[i];
